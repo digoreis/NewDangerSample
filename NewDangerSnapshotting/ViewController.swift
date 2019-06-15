41af486dcd2c9da5da0135695ae2d9a8931053ee
Rodrigo Reis <digo.reis@gmail.com>
2019-06-15 17:53:40 +0100

    Initial Commit

---
+++ NewDangerSnapshotting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewDangerSnapshotting
+//
+//  Created by Rodrigo Reis on 15/06/2019.
+//  Copyright © 2019 Farfetch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
